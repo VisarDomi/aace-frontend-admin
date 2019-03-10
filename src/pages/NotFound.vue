d85dd0ee7af57b97315cf3fdf36d84565e0b3df6
Visar Domi <visardomi4@gmail.com>
2019-03-10 14:07:08 +0100

    centered not found text. needs to be integrated in dashboard layout

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
     <div class="content">
-      <div class="md-layout">
-        <router-link :to="{name: 'Dashboard'}">
-          <h1 class="text-center" id="home">Go to homepage</h1>
-        </router-link>
-        <h1 class="text-center">(Page Not Found)</h1>
-      </div>
+      <router-link :to="{name: 'Dashboard'}">
+        <h1 class="text-center" id="home">Go to homepage</h1>
+      </router-link>
+      <h1 class="text-center">(Page Not Found)</h1>
     </div>
   </div>
 </template>
